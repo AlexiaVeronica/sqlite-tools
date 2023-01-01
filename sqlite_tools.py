8fc23882e37448c27f7afb6552fd15d83d0db4b0
VeronicaAlexia <Elaina-Alex@proton.me>
2023-01-01 14:21:02 +0800

    ~

--- sqlite_tools.py
+++ sqlite_tools.py
@@ -2,7 +2,7 @@ import sqlite3
 import typing
 
 
-class SqliteOrm:
+class SqliteTools:
 
     def __init__(self, db_path):
         self.db_path = db_path
@@ -128,25 +128,4 @@ class SqliteOrm:
 
     def __enter__(self):
         return self
-
-
-if __name__ == '__main__':
-    sql = SqliteOrm("test.db")
-    ins_list = [
-        {"id": "1", "name": "张三", "age": 18},
-        {"id": "2", "name": "李四", "age": 19},
-        {"id": "3", "name": "王五", "age": 20},
-        {"id": "4", "name": "赵六", "age": 21},
-        {"id": "5", "name": "田七", "age": 22},
-        {"id": "6", "name": "孙八", "age": 23},
-        {"id": "7", "name": "周九", "age": 24},
-        {"id": "8", "name": "吴十", "age": 25},
-        {"id": "9", "name": "郑十一", "age": 26},
-        {"id": "10", "name": "王十二", "age": 27},
-    ]
-
-    sql.create_table("test3", ins_list[0].keys())
-    sql.insert_many("test3", ins_list)
-
-    sql.insert("test3", {"id": "11", "name": "王十三", "age": 28})
 
